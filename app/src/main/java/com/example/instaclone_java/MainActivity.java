bc2053342a8e4020202336bac8800e6dba872e9b
saleheen1 <smsaleheen3@gmail.com>
2021-07-02 22:52:38 +0600

    first screen designed

--- app/src/main/java/com/example/instaclone_java/MainActivity.java
+++ app/src/main/java/com/example/instaclone_java/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.instaclone_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iconImage;
+    private LinearLayout loginLinear;
+    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iconImage = findViewById(R.id.id_icon);
+        loginLinear = findViewById(R.id.id_loginLInear);
+        loginBtn = findViewById(R.id.id_login);
+        registerBtn = findViewById(R.id.id_Signup);
+
+        loginLinear.animate().alpha(1f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(1500);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        iconImage.setAnimation(animation);
+
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            loginLinear.setVisibility(View.VISIBLE);
+            loginLinear.animate().alpha(1f).setDuration(3000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
