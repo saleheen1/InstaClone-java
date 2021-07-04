a53238310cb0d9dde7fbdff92c10f79bb5ea7ec5
saleheen1 <smsaleheen3@gmail.com>
2021-07-04 14:19:20 +0600

    register page designed and id initialized

--- app/src/main/java/com/example/instaclone_java/RegisterActivity.java
+++ app/src/main/java/com/example/instaclone_java/RegisterActivity.java
@@ -2,9 +2,16 @@ package com.example.instaclone_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,8 +21,11 @@ public class RegisterActivity extends AppCompatActivity {
     private  EditText usernameInput;
     private EditText emailInput;
     private EditText passwordInput;
+    private Button registerBtn;
+    private TextView alreadyReg;
 
     private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,43 @@ public class RegisterActivity extends AppCompatActivity {
         fullnameInput = findViewById(R.id.nameId);
         emailInput = findViewById(R.id.emailId);
         passwordInput = findViewById(R.id.passwordId);
+        registerBtn = findViewById(R.id.registerBtnId);
+        alreadyReg = findViewById(R.id.alreadyregId);
 
+        //getting instance of Firebase
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //already register button clicked
+        alreadyReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        //clicking on register button
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtUsername = usernameInput.getText().toString();
+                String txtName = fullnameInput.getText().toString();
+                String txtEmail = emailInput.getText().toString();
+                String txtPassword = passwordInput.getText().toString();
+
+
+
+                if(TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtName) || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
+                    Toast.makeText(RegisterActivity.this,"Make sure no field is empty",Toast.LENGTH_SHORT).show();
+                } else if(txtPassword.length() <6){
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 6 character long", Toast.LENGTH_SHORT).show();
+                }else{
+                    //register user
+                    registerUser(String txtUsername,String txtEmail,String txtPassword,String txtName);
+                }
+            }
+        });
     }
+
+
 }
